929902062d5aab26fbfd6ca1feacaa2161fcf08f
Abnel <Abnel@MacBook-Air-de-Abnel.local>
2026-07-02 18:46:57 -0400

    fix: ajusta endpoint de pacientes no app responsavel

--- src/services/pacientes.js
+++ src/services/pacientes.js
@@ -1,12 +1,12 @@
 import { api } from "../lib/api";
 
 export async function listarPacientesResponsavel() {
-  const res = await api.get("/responsavel/pacientes/");
+  const res = await api.get("/responsavel/pacientes");
   return res.data;
 }
 
 export async function listarMeusPacientes() {
-  const res = await api.get("/responsavel/pacientes/");
+  const res = await api.get("/responsavel/pacientes");
   return res.data;
 }
 
